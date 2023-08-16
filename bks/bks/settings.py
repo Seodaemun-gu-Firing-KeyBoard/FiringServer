cdfe86ff356930b9e8a1b4b93443763e34f03f77
haejun1 <a01027833227@gmail.com>
2023-08-16 20:31:36 +0900

    병합

--- bks/bks/settings.py
+++ bks/bks/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     # myapp
     'accounts',
+    'map',
     # django-rest-auth
     'rest_framework',
     'rest_framework_simplejwt',
@@ -55,9 +56,12 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.naver',
     # swagger
     'drf_yasg',
+    #etc
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -218,3 +222,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+]
---
+++ bks/map/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Facility(models.Model):
+    name = models.TextField(blank=True, null=True)
+    target = models.TextField(blank=True, null=True)
+    location = models.TextField(blank=True, null=True)
+    use = models.TextField(blank=True, null=True)
+    reception = models.TextField(blank=True, null=True)
+    select = models.TextField(blank=True, null=True)
+    recruit = models.TextField(blank=True, null=True)
+    application = models.TextField(blank=True, null=True)
+    cancel = models.TextField(blank=True, null=True)
+    fee = models.TextField(blank=True, null=True)
+    reserve = models.TextField(blank=True, null=True)
+    call = models.TextField(blank=True, null=True)
+    address = models.CharField(max_length=255, null=True)
+    type = models.CharField(max_length=100, null=True)
+    typeDetail = models.CharField(max_length=100, null=True)
+    siteUrl = models.TextField(blank=True, null=True)
+    image =  models.TextField(blank=True, null=True)
