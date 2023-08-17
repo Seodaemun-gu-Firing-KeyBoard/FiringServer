525333c753238b13c38d0c721fa2c042495a967e
haejun1 <a01027833227@gmail.com>
2023-08-17 21:09:10 +0900

    충돌, 계정 해결

--- bks/bks/urls.py
+++ bks/bks/urls.py
@@ -23,7 +23,7 @@ from rest_framework import permissions
 schema_view = get_schema_view(
     openapi.Info(
         title="Your API",
-        default_version='v1',
+        default_version="v1",
         description="Your API description",
         terms_of_service="https://www.yourapp.com/terms/",
         contact=openapi.Contact(email="contact@yourapp.com"),
@@ -34,16 +34,17 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('user/', include('user.urls')),
-=======
-    path('api/auth/', include('accounts.urls')),
-    path('api/auth/', include('allauth.urls')),
-
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
->>>>>>> dev2
-    path('map/', include('map.urls')),
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("accounts.urls")),
+    path("api/auth/", include("allauth.urls")),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("map/", include("map.urls")),
 ]
