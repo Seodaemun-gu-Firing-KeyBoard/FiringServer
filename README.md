d0f54514b3e22a35405beb9bbbe412f7194df371
haejun1 <a01027833227@gmail.com>
2023-08-17 18:25:00 +0900

    병합중

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FiringServer
+
+실행전 : pip install -r requirements.txt
--- bks/bks/settings.py
+++ bks/bks/settings.py
@@ -40,13 +40,14 @@ INSTALLED_APPS = [
     "user",
     "rest_framework",
     "map",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -129,3 +130,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
 ]
+
+CORS_ORIGIN_WHITELIST = ("http://127.0.0.1:8000", "http://localhost:3000")
+CORS_ALLOW_CREDENTIALS = True
+
+# settings.py
+CORS_ALLOW_ALL_ORIGINS = True
